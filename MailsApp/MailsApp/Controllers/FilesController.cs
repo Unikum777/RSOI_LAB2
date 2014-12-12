d95a826cb03633c0ab479408bed3610f30e7dbda
Yershov Nike (work) <yershov.n@ail.ru>
2014-12-12 19:01:12 +0300

    First commit http://www.asp.net/web-api/overview/getting-started-with-aspnet-web-api/tutorial-your-first-web-api

---
+++ MailsApp/MailsApp/Controllers/FilesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MailsApp.Models;
+
+namespace MailsApp.Controllers
+{
+    public class FilesController : ApiController
+    {
+	    private SkyDriveFile[] files = new SkyDriveFile[]
+	    {
+		    new SkyDriveFile
+		    {
+			    Access = "Just Me",
+			    FileId = "01",
+			    FileName = "Vasya",
+			    Id = "01",
+			    Link = "http\01",
+			    ParentId = "0",
+			    Size = 995
+		    },
+		    new SkyDriveFile
+		    {
+			    Access = "Just Me",
+			    FileId = "02",
+			    FileName = "Petya",
+			    Id = "01",
+			    Link = "http\02",
+			    ParentId = "1",
+			    Size = 995
+		    },
+		    new SkyDriveFile
+		    {
+			    Access = "Just Me",
+			    FileId = "03",
+			    FileName = "Masha",
+			    Id = "01",
+			    Link = "http\03",
+			    ParentId = "1",
+			    Size = 995
+		    }
+	    };
+
+	    public IEnumerable<SkyDriveFile> GetAllFiles()
+	    {
+		    return files;
+	    }
+
+		public IHttpActionResult GetProduct(string id)
+		{
+			var product = files.FirstOrDefault((p) => p.Id == id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			return Ok(product);
+		}
+    }
+}
---
+++ MailsApp/MailsApp/Models/SkyDriveFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MailsApp.Models
+{
+	public class SkyDriveFile
+	{
+		public string Id { get; set; }
+		public string FileName { get; set; }
+		public string FileId { get; set; }
+		public string ParentId { get; set; }
+		public int Size { get; set; }
+		public string Link { get; set; }
+		public string Access { get; set; }
+	}
+}
